46c1debac6d0afc07edb8b416468b9ed6c95fa12
jeongEY <kingship89@naver.com>
2023-07-13 14:56:41 +0900

    add props

--- App.js
+++ App.js
@@ -1,14 +1,56 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, FlatList, StyleSheet, TextInput, View } from "react-native";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [todoGoals, setTodoGoals] = useState([]);
+
+  //사용자가 내용을 입력할 때 해당 입력값을 가져오는 함수
+  const goalInputHandler = (enteredText) => {
+    //console.log(enteredText);
+    setEnteredGoalText(enteredText);
+  };
+
+  //버튼을 누르면 할 일 목록을 추가하는 함수
+  const addGoalHandler = () => {
+    //console.log(enteredGoalText);
+
+    //useState로 관리하는 상태변수의 setter 안에 콜백 함수를 작성하면,
+    //그 콜백 함수의 매개값은 항상 해당 상태 변수의 최신 값이 전달된다.
+    setTodoGoals((currentTodoGoals) => [
+      ...currentTodoGoals,
+      { text: enteredGoalText, key: Math.random().toString() },
+    ]);
+    console.log(todoGoals);
+  };
+
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="할 일을 입력하세요!" />
-        <Button title="할 일 추가하기" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="할 일을 입력하세요!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="할 일 추가하기" onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>할 일 목록들...</Text>
+      <View style={styles.goalsContainer}>
+        {/* ScrollView는 전체 화면이 렌더링 될 때 안의 항목들을 전부 렌더링한다.
+            이로인해, 성능의 저하가 발생할 수 있다.
+            (보이지 않는 영역까지 렌더링을 진행하기 때문에 목록이 많다면 로딩이 길어진다.)
+            FlatList는 보이는 영역만 일단 렌더링을 진행하고, 나머지 항목들은
+            스크롤 움직임이 발생하면 렌더링을 진행한다. */}
+        <FlatList
+          data={todoGoals}
+          renderItem={(itemData) => {
+            return <GoalItem itemData={itemData} />;
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceHorizontal={false}
+        ></FlatList>
       </View>
     </View>
   );
@@ -16,6 +58,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 5,
     padding: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "68%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
---
+++ components/GoalItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const GoalItem = ({ itemData }) => {
+  return (
+    <View style={styles.goalItem}>
+      <Text style={styles.goalText}>{itemData.item.text}</Text>
+    </View>
+  );
+};
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
+  },
+  goalText: {
+    color: "white",
+  },
+});
