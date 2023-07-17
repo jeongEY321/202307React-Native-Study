2ec982736fc8981d50a9490bde3d272bcc95bde6
jeongEY <kingship89@naver.com>
2023-07-17 15:55:27 +0900

    GoalInput 컴포넌트 분리, 삭제 로직 작성 중...

--- App.js
+++ App.js
@@ -1,55 +1,53 @@
 import { useState } from "react";
 import { Button, FlatList, StyleSheet, TextInput, View } from "react-native";
+
 import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState("");
   const [todoGoals, setTodoGoals] = useState([]);
 
-  //사용자가 내용을 입력할 때 해당 입력값을 가져오는 함수
-  const goalInputHandler = (enteredText) => {
-    //console.log(enteredText);
-    setEnteredGoalText(enteredText);
-  };
-
   //버튼을 누르면 할 일 목록을 추가하는 함수
-  const addGoalHandler = () => {
-    //console.log(enteredGoalText);
-
-    //useState로 관리하는 상태변수의 setter 안에 콜백 함수를 작성하면,
-    //그 콜백 함수의 매개값은 항상 해당 상태 변수의 최신 값이 전달된다.
+  const addGoalHandler = (enteredGoalText) => {
+    //useState로 관리하는 상태 변수의 setter 안에 콜백 함수를 작성하면,
+    //그 콜백 함수의 매개값은 항상 해당 상태 변수의 최신 값이 전달됩니다.
     setTodoGoals((currentTodoGoals) => [
       ...currentTodoGoals,
-      { text: enteredGoalText, key: Math.random().toString() },
+      { text: enteredGoalText, id: Math.random().toString() },
     ]);
-    console.log(todoGoals);
+  };
+
+  const deleteGoalHandler = (id) => {
+    setTodoGoals((currentTodoGoals) => {
+      return currentTodoGoals.filter((goal) => goal.id !== id);
+    });
   };
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="할 일을 입력하세요!"
-          onChangeText={goalInputHandler}
-        />
-        <Button title="할 일 추가하기" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
-        {/* ScrollView는 전체 화면이 렌더링 될 때 안의 항목들을 전부 렌더링한다.
-            이로인해, 성능의 저하가 발생할 수 있다.
-            (보이지 않는 영역까지 렌더링을 진행하기 때문에 목록이 많다면 로딩이 길어진다.)
+        {/* ScrollView는 전체 화면이 렌더링 될 때 안의 항목들을 전부 렌더링합니다.
+            이로 인해, 성능의 저하가 발생할 수 있습니다.
+            (보이지 않는 영역까지 렌더링을 진행하기 때문에 목록이 많다면 로딩이 길어짐.) 
             FlatList는 보이는 영역만 일단 렌더링을 진행하고, 나머지 항목들은
-            스크롤 움직임이 발생하면 렌더링을 진행한다. */}
+            스크롤 움직임이 발생하면 렌더링을 진행합니다. */}
         <FlatList
           data={todoGoals}
           renderItem={(itemData) => {
-            return <GoalItem itemData={itemData} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
+            // console.log("item: ", item);
+            // console.log("index: ", index);
             return item.id;
           }}
-          alwaysBounceHorizontal={false}
+          alwaysBounceVertical={false}
         ></FlatList>
       </View>
     </View>
@@ -58,25 +56,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    flex: 5,
-    padding: 50,
-    paddingHorizontal: 16,
-  },
-  inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "68%",
-    marginRight: 8,
-    padding: 8,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   goalsContainer: {
     flex: 4,
